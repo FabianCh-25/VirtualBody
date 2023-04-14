ac579e9e86ceee4effb62441849d072b5c3aa702
Fabian Chagua <chaguamartinkevin@gmail.com>
2023-04-13 21:56:38 -0500

    el listar actividades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActividadListarComponent } from './component/actividad/actividad-listar/actividad-listar.component';
+import { ActividadComponent } from './component/actividad/actividad.component';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActividadListarComponent,
+    ActividadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/actividad.service.ts
+++ src/app/service/actividad.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment'; // tienes q importar
+import { HttpClient } from '@angular/common/http'; // tienes q importar
+import { Actividad } from '../model/actividad'; // tienes q importar
 
+const base_url=environment.base
 @Injectable({
   providedIn: 'root'
 })
 export class ActividadService {
+  private url=`${base_url}/actividades`
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  list(){
+    return this.http.get<Actividad[]>(this.url)
+  }
 }
