fba2649923e7b087d59ec16df01a2d28d4bcfde7
Fabian Chagua <chaguamartinkevin@gmail.com>
2023-04-13 20:12:28 -0500

    agregamos carpetas model service environments

---
+++ src/app/model/actividad.ts
@@ -0,0 +1,7 @@
+export class Actividad {
+  CodigoActividad:number=0
+  Titulo:string= ""
+  Descripcion: string =""
+  FechaPublicacion: Date = new Date(Date.now())
+  FechaEntrega: Date = new Date(Date.now())
+}
