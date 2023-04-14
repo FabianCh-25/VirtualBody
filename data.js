b2fc71648e21eb807ba8c8d582515e432671ba51
Fabian Chagua <chaguamartinkevin@gmail.com>
2023-04-13 20:12:02 -0500

    creamos fake data

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+
+  var data = {
+    actividades: [
+      {
+        CodigoActividad:1,
+        Titulo: "Tobby",
+        Descripcion: "Gaaaaaaa",
+        FechaPublicacion:"2022-09-10",
+        FechaEntrega:"2022-09-10",
+
+      }
+    ]
+  }
+  return data
+  }
