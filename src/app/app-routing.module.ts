7012607f64872388fd6d81b52c953640abac268c
RonaldCM <u201920185@upc.edu.pe>
2023-04-15 22:25:59 -0500

    creacion de los componentes listar, crear y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearCursosComponent } from './crear-cursos/crear-cursos.component';
+import { ListarCursosComponent } from './listar-cursos/listar-cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crear-cursos', component: CrearCursosComponent },
+  { path: 'listar-cursos', component: ListarCursosComponent },
+  { path: '', redirectTo: '/listar-curso', pathMatch: 'full' },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/crear-cursos/crear-cursos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso.model'; // Importa el modelo de curso
+import { CursosService } from '../cursos.service'; // Importa el servicio
+
+@Component({
+  selector: 'app-crear-cursos',
+  templateUrl: './crear-cursos.component.html',
+  styleUrls: ['./crear-cursos.component.css']
+})
+export class CrearCursosComponent implements OnInit {
+  CodigoCurso: number;
+  NombreCurso: string;
+  Descripcion: string;
+
+  constructor(private cursosService: CursosService) { // Inyecta el servicio
+    this.CodigoCurso = 0;
+    this.NombreCurso = '';
+    this.Descripcion = '';
+  }
+
+  ngOnInit(): void {
+  }
+
+  // Método para agregar un curso utilizando el servicio
+  agregarCurso(): void {
+    const nuevoCurso = new Curso(this.CodigoCurso, this.NombreCurso, this.Descripcion);
+    this.cursosService.agregarCurso(nuevoCurso);
+    this.CodigoCurso = 0;
+    this.NombreCurso = '';
+    this.Descripcion = '';
+  }
+}
+
