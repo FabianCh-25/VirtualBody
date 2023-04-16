45f92de0a7aaba98cb48171fe7cc557a1268be62
Fabian Chagua <chaguamartinkevin@gmail.com>
2023-04-15 23:25:32 -0500

    creación de components para insertar activiad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActividadListarComponent } from './component/actividad/actividad-listar/actividad-listar.component';
 import { ActividadComponent } from './component/actividad/actividad.component';
 import { MatTableModule } from '@angular/material/table';
+import { ActividadInsertarComponent } from './component/actividad/actividad-insertar/actividad-insertar.component';
 @NgModule({
   declarations: [
     AppComponent,
     ActividadListarComponent,
-    ActividadComponent
+    ActividadComponent,
+    ActividadInsertarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/actividad/actividad-insertar/actividad-insertar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actividad-insertar',
+  templateUrl: './actividad-insertar.component.html',
+  styleUrls: ['./actividad-insertar.component.css']
+})
+export class ActividadInsertarComponent {
+
+}
