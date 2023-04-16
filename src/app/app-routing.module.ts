59efef2d49ee53ef3a05737f9a617a9c4316f9f8
RonaldCM <u201920185@upc.edu.pe>
2023-04-15 22:35:05 -0500

    agregar a nvar (listar actividad)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearCursosComponent } from './crear-cursos/crear-cursos.component';
 import { ListarCursosComponent } from './listar-cursos/listar-cursos.component';
+import { ActividadListarComponent } from './component/actividad/actividad-listar/actividad-listar.component';
 
 const routes: Routes = [
   { path: 'crear-cursos', component: CrearCursosComponent },
   { path: 'listar-cursos', component: ListarCursosComponent },
+  { path: 'actividad-listar', component: ActividadListarComponent },
   { path: '', redirectTo: '/listar-curso', pathMatch: 'full' },
 ];
 
