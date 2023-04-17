b88e3eea06574c2ce9f4c350d44aeb325a460c7f
RonaldCM <u201920185@upc.edu.pe>
2023-04-16 23:57:41 -0500

    arreglo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,26 @@ import { CrearCursosComponent } from './crear-cursos/crear-cursos.component';
 import { ListarCursosComponent } from './listar-cursos/listar-cursos.component';
 import { ActividadListarComponent } from './component/actividad/actividad-listar/actividad-listar.component';
 import { ActividadInsertarComponent } from './component/actividad/actividad-insertar/actividad-insertar.component';
+import { AulaInsertarComponent } from './component/aula/aula-insertar/aula-insertar.component';
+import { AulaListarComponent } from './component/aula/aula-listar/aula-listar.component';
+import { DocenteInsertarComponent } from './component/docente/docente-insertar/docente-insertar.component';
+import { DocenteListarComponent } from './component/docente/docente-listar/docente-listar.component';
+import { EstudianteInsertarComponent } from './component/estudiante/estudiante-insertar/estudiante-insertar.component';
+import { EstudianteListarComponent } from './component/estudiante/estudiante-listar/estudiante-listar.component';
 
 const routes: Routes = [
   { path: 'crear-cursos', component: CrearCursosComponent },
   { path: 'listar-cursos', component: ListarCursosComponent },
+  { path: 'actividad-insertar', component: ActividadInsertarComponent },
   { path: 'actividad-listar', component: ActividadListarComponent },
+  { path: 'aula-insertar', component: AulaInsertarComponent },
+  { path: 'aula-listar', component: AulaListarComponent },
+  { path: 'docente-insertar', component: DocenteInsertarComponent },
+  { path: 'docente-listar', component: DocenteListarComponent },
+  { path: 'estudiante-insertar', component: EstudianteInsertarComponent },
+  { path: 'estudiante-listar', component: EstudianteListarComponent },
   { path: '', redirectTo: '/listar-curso', pathMatch: 'full' },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
