8b17326cc84644309e24a93202c7a9bf04d479db
tu nombre de usuario <u20201f777@upc.edu.pe>
2023-04-16 20:56:58 -0500

    prueba_aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ActividadListarComponent } from './component/actividad/actividad-listar/actividad-listar.component';
 import { ActividadComponent } from './component/actividad/actividad.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { AulaListarComponent } from './component/aula/aula-listar/aula-listar.component';
+import { AulaComponent } from './component/aula/aula.component';
+import { AulaInsertarComponent } from './component/aula/aula-insertar/aula-insertar.component';
 @NgModule({
   declarations: [
     AppComponent,
     ActividadListarComponent,
-    ActividadComponent
+    ActividadComponent,
+    AulaListarComponent,
+    AulaComponent,
+    AulaInsertarComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +31,13 @@ import { MatTableModule } from '@angular/material/table';
     HttpClientModule,
     BrowserAnimationsModule,
     MatTableModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
