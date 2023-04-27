caa6f03cf6f7bf9b7f750d7c54e2eba1345abb93
RonaldCM <u201920185@upc.edu.pe>
2023-04-26 19:52:06 -0500

    CAMBIOS

--- data.js
+++ data.js
@@ -10,7 +10,20 @@ module.exports = function () {
         FechaEntrega:"2022-09-10",
 
       }
-    ]
+    ],
+
+    cursos : [
+      {CodigoCurso: 1, NombreCurso: "Programacion Web", Descripcion: "Aprenderemos a programar en la web"},
+      {CodigoCurso: 2, NombreCurso: "Programacion Movil", Descripcion: "Aprenderemos a programar en la movil"},
+      {CodigoCurso: 3, NombreCurso: "Programacion Escritorio", Descripcion: "Aprenderemos a programar en la escritorio"},
+      {CodigoCurso: 4, NombreCurso: "Programacion Web", Descripcion: "Aprenderemos a programar en la web"},
+      {CodigoCurso: 5, NombreCurso: "Programacion Movil", Descripcion: "Aprenderemos a programar en la movil"},
+      {CodigoCurso: 6, NombreCurso: "Programacion Escritorio", Descripcion: "Aprenderemos a programar en la escritorio"},
+      {CodigoCurso: 7, NombreCurso: "Programacion Web", Descripcion: "Aprenderemos a programar en la web"},
+      {CodigoCurso: 8, NombreCurso: "Programacion Movil", Descripcion: "Aprenderemos a programar en la movil"},
+      {CodigoCurso: 9, NombreCurso: "Programacion Escritorio", Descripcion: "Aprenderemos a programar en la escritorio"},
+
+  ]
   }
   return data
   }
